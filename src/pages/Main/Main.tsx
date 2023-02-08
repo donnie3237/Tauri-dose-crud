95b34b9cf4484734a584b992fac29ab4c176db9c
pardorn3237 <paradorn3237@gmail.com>
2023-02-08 22:14:34 +0700

    .

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './FP.scss'
 function Main() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
   return (
+    
     <div className='FP main'>
+      
         <p>Welcome to DosE-CRUD</p>
+        <h1>{isOnline? "onlne":"not online"}</h1>
         <div className="flex fr"><h1>1</h1></div>
         <div className="flex mini">
           <div className="flex on"><h1>2</h1></div>
